1ff04f8a07087661a33aa6dd1ec3e72602ff87e8
pathik10patel <pathik10patel@gmail.com>
2018-10-09 09:04:59 +0530

    first commit

---
+++ jenkintaskconsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jenkintaskconsole
+{
+    public class Program
+    {
+        public static int sum()
+        {
+            int i, sum = 0;
+            for (i = 0; i <= 10; i++)
+            {
+                sum += i;
+            }
+            //Console.Write("The sum of 10 no is : {0}", sum);
+            return sum;
+        }
+
+        public static void Main(string[] args)
+        {
+            int x = sum();
+            Console.WriteLine(x);
+        }
+
+       
+    }
+
+}
